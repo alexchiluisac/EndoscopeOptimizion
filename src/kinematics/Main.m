377f8680dba80c8a090035ec850f4c6581685302
kid-a <loris.fichera@gmail.com>
2019-03-01 12:24:18 -0500

    moved wrist definition to a new class. Changed the arc-associated HT matrix in the forward kinematics to make the robot bend in the X axis when no base rotation is applied. Minor improvements to plot clarity.

--- src/kinematics/Main.m
+++ src/kinematics/Main.m
@@ -1,10 +1,11 @@
 %% Robot endoscope features.
-clc; clear; close all;
+clc, clear, close all
 addpath('../utils')
+
 cutouts(1).w = 1;
 cutouts(1).u = 1;
 cutouts(1).h = 1;
 cutouts(1).alpha = 0;
-r = Robot(1.6,1.85,3,cutouts)
-% T = r.fwkine([0.5, 0, 0])
-teach(r,[0.5, 0, 0])
+
+robot = Wrist(1.6, 1.85, 3, cutouts);
+teach(robot, [0.2, 0, 0]);
--- src/kinematics/teach.m
+++ src/kinematics/teach.m
@@ -1,18 +1,27 @@
-
-
 function H = teach(robot, configuration)
-[P, T] = robot.fwkine(configuration);
+% TEACH moves a robot to a given configuration and plots the corresponding
+% pose.
+%
+%
+% Authors: A. Chiluisa <ajchiluisa@wpi.edu>
+%          L. Fichera  <lfichera@wpi.edu>
+%
+% Last revision: 3/1/2019
 
-x = P(1,:);
-y = P(2,:);
-z = P(3,:);
-scatter3(x,y,z,'r');
-hold on, axis equal
-plot3(x,y,z,'k');
-xlabel('X[m]')
-ylabel('Y[m]')
-zlabel('Z[m]')
+  [P, T] = robot.fwkine(configuration);
 
-triad
-triad('Matrix',T(:,:,end))
+  X = P(1,:);
+  Y = P(2,:);
+  Z = P(3,:);
+  
+  scatter3(X, Y, Z, 100, 'r', 'filled');
+  hold on, axis equal
+  
+  plot3(X, Y, Z, 'k', 'LineWidth', 2.5);
+  xlabel('X[m]')
+  ylabel('Y[m]')
+  zlabel('Z[m]')
+  
+  triad('Matrix', eye(4), 'linewidth', 2.5);
+  triad('Matrix', T(:,:,end), 'linewidth', 2.5);
 end
