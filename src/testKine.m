b92c48c636227fd4e9939aeae40e9424b8869325
kid-a <loris.fichera@gmail.com>
2019-03-07 14:18:27 -0500

    added function to test the kinematics

---
+++ src/testKine.m
@@ -0,0 +1,13 @@
+%% Robot endoscope features.
+clc, clear, close all
+addpath('kinematics')
+addpath('path-planning')
+addpath('utils')
+
+cutouts(1).w = 1;
+cutouts(1).u = 1;
+cutouts(1).h = 1;
+cutouts(1).alpha = 0;
+
+robot = Wrist(1.6, 1.85, 3, cutouts);
+teach(robot, [0.2, pi/2, 0]);
