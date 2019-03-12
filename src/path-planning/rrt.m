c9fdceeeb9a8514dd885df9732032dd718de180b
alexchiluisac <44651550+alexchiluisac@users.noreply.github.com>
2019-03-12 09:47:26 -0400

    Update rrt.m

--- src/path-planning/rrt.m
+++ src/path-planning/rrt.m
@@ -12,7 +12,7 @@ function [qListNormalized,qList,pList,aList] = rrt(robot, qbounds, anatomyModel)
         collisionDetection = true;
     end
     
-    nPoints = 1000;
+    nPoints = 500;
     
     % algorithm parameters
     deltaQ = [0.05 0.05 0.05]; % step
@@ -26,15 +26,21 @@ function [qListNormalized,qList,pList,aList] = rrt(robot, qbounds, anatomyModel)
     pList = zeros(3, nPoints);
     aList = zeros(3, nPoints);
     
+ 
     % initialize the base transform
     T_robot_in_env = anatomyModel.baseTransform;
-    
+ 
+    [P,T] = robot.fwkine(qList(:,1), T_robot_in_env);
+    pList(:,1) = T(1:3,4,end);
+    aList(:,1) = T(1:3,3,end);
     % iteratively build the tree
     hw = waitbar(0, 'Sampling the configuration space. Please wait...');
     
+    jj = 2;
+    
     for ii = 1 : nPoints
         qRand = rand(3,1);
-        qNearest = nearestVertex(qRand, qListNormalized, ii);
+        qNearest = nearestVertex(qRand, qListNormalized, jj);
         qNew = move(qNearest, qRand, deltaQ);
         
         % Check for potential collisions
@@ -44,7 +50,7 @@ function [qListNormalized,qList,pList,aList] = rrt(robot, qbounds, anatomyModel)
 %         
         [P,T] = robot.fwkine([displ, rot, adv], T_robot_in_env);
 %         P = applytransform(P, T_robot_in_env); %!FIXME T_robot_in_env needs to be calculated
-        robotPM = robot.makePhysicalModel(qList(:,500), T_robot_in_env); %!FIXME T_robot_in_env needs to be calculated
+        robotPM = robot.makePhysicalModel([displ, rot, adv], T_robot_in_env); %!FIXME T_robot_in_env needs to be calculated
         
         testpts = [robotPM.surface.X(:) robotPM.surface.Y(:) robotPM.surface.Z(:)];
         collision = intriangulation(anatomyModel.vertices, ...
@@ -53,21 +59,29 @@ function [qListNormalized,qList,pList,aList] = rrt(robot, qbounds, anatomyModel)
         collision = sum(collision);
         
         if collision > 1
+            disp('Collision');
             continue;
         end
          
         % If no collision, add this point to the tree
-        qListNormalized(:,ii) = qNew;
+        qListNormalized(:,jj) = qNew;
         
-        qList(1,ii) = displ;  
-        qList(2,ii) = rot;
-        qList(3,ii) = adv;
+        qList(1,jj) = displ;  
+        qList(2,jj) = rot;
+        qList(3,jj) = adv;
         
-        pList(:,ii) = T(1:3,4,end);
-        aList(:,ii) = T(1:3,3,end);
+        pList(:,jj) = T(1:3,4,end);
+        aList(:,jj) = T(1:3,3,end);
+        
+        jj = jj + 1;
         
         waitbar(ii/nPoints, hw, 'Sampling the configuration space. Please wait...');
     end
     
     close(hw);
+    
+    qListNormalized = qListNormalized(:,1:jj);
+    qList = qList(:,1:jj);
+    pList = pList(:,1:jj);
+    aList = aList(:,1:jj);
 end
