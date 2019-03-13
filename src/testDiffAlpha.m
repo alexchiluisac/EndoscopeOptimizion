a8b649142ad88be0791c1b510bb318acc1f8354b
Loris Fichera <loris.fichera@gmail.com>
2019-03-13 16:17:16 -0400

    Update testDiffAlpha.m

--- src/testDiffAlpha.m
+++ src/testDiffAlpha.m
@@ -30,9 +30,10 @@ earModel.baseTransform = T;
 reachableVolume = zeros(5,1);
 i = 1;
 
-for alpha = 0:pi/4:pi
-    
-    cutouts.alpha = [0 0 alpha 0];
+alpha = 0 : pi/4 : pi;
+
+for a = alpha
+    cutouts.alpha = [0 0 a 0];
     % Create the robot
     robot = Wrist(1.6, 1.85, 4, cutouts);
     
@@ -41,24 +42,16 @@ for alpha = 0:pi/4:pi
         earModel);
     
     [k,v] = boundary(pList(1,:)', pList(2,:)', pList(3,:)', 0.5);
-    %angles(i,1) = alpha;
     reachableVolume(i,1) = v;
     i = i + 1;
     
 end
 
 % Plot objective function
-angles = alpha * 180 / pi;
-
 figure
-plot(angles,reachableVolume)
-axis equal, grid on
-xlabel('Angle [Degree]'), ylabel('Reachable Volume [mm3]');
-
-figure 
 scatter(alpha * 180 / pi, reachableVolume)
 %axis equal
 grid on
-xlabel('Angle [Degree]'), ylabel('Reachable Volume [mm3]');
-xlim([alpha(1) alpha(end)] * 180 / pi)
-ylim([min(reachableVolume) max(reachableVolume)])
+xlabel('Angle [Degree]'), ylabel('Reachable Volume [mm^3]');
+xlim([alpha(1) alpha(end)]  * 180 / pi)
+ylim([0 max(reachableVolume)])
