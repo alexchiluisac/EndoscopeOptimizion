0e243d7153c865fbf31319b34d02a5b568c9880a
alexchiluisac <44651550+alexchiluisac@users.noreply.github.com>
2019-03-25 09:27:43 -0400

    Test simulating annealing. Minimize the objective function

---
+++ src/testoptimization.m
@@ -0,0 +1,18 @@
+clc; close all; clear all;
+% alpha = input('Ingress the angle value [rad]: ');
+% x1 = -20:0.5:20;
+% x2 = -20:0.5:20;
+% 
+% y = zeros(length(x1), length(x2));
+% 
+% for ii = 1 : length(x1)
+%     for jj = 1 : length(x2)
+%         y(ii,jj) = testModel([x1(ii) x2(jj)]); 
+%     end
+% end
+% alpha = 0;
+% estimateVolume = testModel(alpha)
+ObjectiveFunction = @testModel;
+alpha0 = 0;   % Starting point
+rng default % For reproducibility
+[x,fval,exitFlag,output] = simulannealbnd(ObjectiveFunction,alpha0)
