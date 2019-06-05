08e9a75b2dc1276e188a9085d8b8e6810c658e01
Floris van Rossum <florisvanrossum@gmail.com>
2019-06-05 14:57:43 -0400

    Working state, editing and designing is functional, now doing some cleaning and UI improvements

--- src/gui/drawNotched.m
+++ src/gui/drawNotched.m
@@ -10,36 +10,39 @@ od = app.wrist.OD; % Outer dimension of the wrist
 counter = 1;
 previousZ = 0;
 
+% Iterate through all the cuts
 while counter <= app.wrist.nCutouts
     %% Draw the tube
-    disp(counter);
     [X,Y,Z] = cylinder(od);
     Z(1,:) = previousZ;
     Z(2,:) = cuts.u(counter) + previousZ;
-    mesh(ax, X, Y, Z ,'facecolor',[0.5 1 0])
-
-    %% Draw the cut-out
-    [theta, rho] = cart2pol(X, Y);
-    alpha = cuts.alpha(counter);
+    mesh(ax, X, Y, Z ,'facecolor', '#2f343d', 'edgecolor', '#6d7077')
     
-    alpha = mod(alpha, pi);
-    
-    if alpha < 0
-        alpha = alpha + pi;
+    %% Draw the cut-out semi-circle
+    if counter ~= app.wrist.nCutouts
+        [theta, rho] = cart2pol(X, Y);
+        alpha = cuts.alpha(counter);
+        
+        alpha = mod(alpha, pi);
+        
+        if alpha < 0
+            alpha = alpha + pi;
+        end
+        
+        minAlpha = alpha;
+        
+        maxAlpha = minAlpha - pi;
+        
+        theta( theta < minAlpha & theta > maxAlpha) = minAlpha;
+        
+        [X, Y] = pol2cart(theta, rho);
+        
+        Z(1,:) = cuts.u(counter) + previousZ;
+        Z(2,:) = cuts.h(counter) + cuts.u(counter) + previousZ;
+        surf(ax, X, Y, Z, 'facecolor', '#2f343d', 'edgecolor', '#6d7077');
+        previousZ = cuts.h(counter) + cuts.u(counter) + previousZ;
+        
     end
-    
-    minAlpha = alpha 
-    
-    maxAlpha = minAlpha - pi;
-    
-    theta( theta < minAlpha & theta > maxAlpha) = minAlpha
-
-    [X, Y] = pol2cart(theta, rho);
-    
-    Z(1,:) = cuts.u(counter) + previousZ;
-    Z(2,:) = cuts.h(counter) + cuts.u(counter) + previousZ;
-    surf(ax, X, Y, Z, 'facecolor', [0 1 1]);
-    previousZ = cuts.h(counter) + cuts.u(counter) + previousZ;
     counter = counter + 1;
 end
 end
