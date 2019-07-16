b541193a0837c56dd9b2969efa2eb2d2e8436c43
Floris van Rossum <florisvanrossum@gmail.com>
2019-07-16 15:05:54 -0400

    Creating simple UI for navigation

---
+++ src/runSynthesisApp.m
@@ -0,0 +1,18 @@
+function runSynthesisApp()
+%% Main function for Notched Tube Designer App
+% Created by: Floris van Rossum, 5/28/2019
+% COgnitive MEdical Technology Robotics Laboratory (COMET Robotics Lab)
+% Worcester Polytechnic Institute
+
+%% Program Configuration
+% Close all previous windows and add folders, sub-folders to path
+
+addpath('utils');
+addpath('utils/stlTools/');
+addpath('kinematics'); 
+addpath('gui');
+addpath("anatomical-models");
+
+app = SynthesisApp();
+
+end
