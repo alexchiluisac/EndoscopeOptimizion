b7d4c91431d134b160dd648ad4501b49681921c4
Loris Fichera <loris@agostino.local>
2019-09-06 11:51:31 -0400

    fixed small mistake in the calculation of the neutral bending plane

--- src/kinematics/Wrist.m
+++ src/kinematics/Wrist.m
@@ -54,7 +54,7 @@ classdef Wrist < Robot
                 phii = 2 * acos(d(ii) / ri);
                 
                 ybaro = (4 * ro * (sin(0.5 * phio)) ^ 3)/ (3 * (phio - sin(phio)));
-                ybari = (4 * ri * (sin(0.5 * phii)) ^ 3)/ (3 * (phio - sin(phii)));
+                ybari = (4 * ri * (sin(0.5 * phii)) ^ 3)/ (3 * (phii - sin(phii)));
                 
                 Ao = ( (ro ^ 2) * ( phio - sin(phio))) / 2;
                 Ai = ( (ri ^ 2) * ( phii - sin(phii))) / 2;
